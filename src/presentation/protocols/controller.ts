3ea4fc4a7b44a12895ee43d134f6d9848bc1780e
lcs13761@gmail.com <lcs13761@gmail.com>
2022-01-26 13:02:33 -0300

    refactor: transform handle to async

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -2,5 +2,5 @@ import { HttpResponse, HttpRequest } from './http'
 
 export interface Controller {
   // eslint-disable-next-line @typescript-eslint/method-signature-style
-  handle (httpRequest: HttpRequest): HttpResponse
+  handle (httpRequest: HttpRequest): Promise<HttpResponse>
 }
