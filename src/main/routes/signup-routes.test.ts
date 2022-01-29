2a4f44aa06d9188012057c7b954babb56fe5c9c1
lcs13761@gmail.com <lcs13761@gmail.com>
2022-01-29 17:25:58 -0300

    feat: connect to mongodb before app listen

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,16 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => { await MongoHelper.connect(process.env.MONGO_URL as string) })
+
+  afterAll(async () => { await MongoHelper.disconnect() })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
+  })
   test('should retrun an account on success', async () => {
     await request(app).post('/api/signup').send({
       name: 'test',
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,8 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helpers'
 
-app.listen(5050, () => console.log('server Running'))
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => console.log('server Running'))
+}).catch(console.error)
