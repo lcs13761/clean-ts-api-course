0c4f1e7f533eaa053935489631f9c7ce77d7c08a
lcs13761@gmail.com <lcs13761@gmail.com>
2022-01-29 15:57:40 -0300

    feat: ensure requests return json as default content type and cors middlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,9 @@
 import { Express, json } from 'express'
+import { cors } from '../middlewares/cors'
+import { contentType } from '../middlewares/content-type'
 
 export default (app: Express): void => {
   app.use(json())
+  app.use(cors)
+  app.use(contentType)
 }
